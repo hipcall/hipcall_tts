e97e799e701ae61085e9bd8482467012228aa11a
Meliha Çakmak <meliha.cakmak@bulutfon.com>
2026-01-20 16:56:13 +0300

    feat: Implement Elevenlabs provider

--- config/config.exs
+++ config/config.exs
@@ -5,6 +5,9 @@ config :hipcall_tts, :providers,
     api_key: {:system, "OPENAI_API_KEY"},
     base_url: "https://api.openai.com"
   ],
+  elevenlabs: [
+    api_key: {:system, "ELEVENLABS_API_KEY"}
+  ],
   polly: [
     access_key_id: {:system, "AWS_ACCESS_KEY_ID"},
     secret_access_key: {:system, "AWS_SECRET_ACCESS_KEY"}
